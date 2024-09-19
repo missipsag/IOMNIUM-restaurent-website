f61202b7be96089718e37f21cd014666cc3edabf
missipsa ghernaout <ghernaoutmissipsa.pro@gmail.com>
2024-09-19 12:11:19 +0200

    add the static files and productsRoutes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.use("/IOMNIUM", productRoutes);
 
 //set up the static file directory 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static( path.join(__dirname, 'public')));
 
 
 
